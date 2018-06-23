9afee8c0d9aba3e07d9e2c44dd5bbe81fa42bd06
Corbett Martin <corbtastik@gmail.com>
2018-06-23 12:52:35 -0500

    maintenance

--- src/main/java/io/corbs/TodosCacheApp.java
+++ src/main/java/io/corbs/TodosCacheApp.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
